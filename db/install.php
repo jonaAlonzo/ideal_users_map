981e129b29e79cc624658cdc9e56eec45630a2f2
Jona Alonzo <jona.job.865@gmail.com>
2024-11-18 14:31:11 +0100

    uptade registro db

--- db/install.php
+++ db/install.php
@@ -12,9 +12,27 @@ function xmldb_block_ideal_users_map_install()
     $transaction = $DB->start_delegated_transaction();
 
     try {
-        foreach ($cities as $cityGroup) {
+        // Verificar si la tabla 'ideal_users_map' existe
+        if ($DB->get_manager()->table_exists('ideal_users_map')) {
+            // Actualizar registros existentes
+            foreach ($cities as $cityGroup) {
+            foreach ($cityGroup as $city) {
+                // Verificar si el registro ya existe
+                $existingRecord = $DB->get_record('ideal_users_map', array('id' => $city->id));
+                if ($existingRecord) {
+                // Actualizar el registro existente
+                $DB->update_record('ideal_users_map', $city);
+                } else {
+                // Insertar nuevo registro
+                $DB->insert_record('ideal_users_map', $city);
+                }
+            }
+            }
+        } else {
             // Insertar el grupo de registros directamente
+            foreach ($cities as $cityGroup) {
             $DB->insert_records('ideal_users_map', $cityGroup);
+            }
         }
 
         // Permitir el commit de la transacción después de ejecutar todas las consultas
