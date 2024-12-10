c6c8d98942ac1acb21f607955b8af812a04f26e3
Jona Alonzo <jona.job.865@gmail.com>
2024-12-10 10:51:49 +0100

    presentacion de datos segun plataforma(moodle) e IP user v2

--- version.php
+++ version.php
@@ -2,5 +2,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_ideal_users_map';
-$plugin->version = 2024102001.3; // YYYYMMDDXX format
+$plugin->version = 2024102001.4; // YYYYMMDDXX format
 $plugin->requires = 2021051700; // Moodle 3.11
