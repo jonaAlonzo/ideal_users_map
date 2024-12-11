1e054d13c6cbe7471ccb13a2d531e2e7067747e8
Jona Alonzo <jona.job.865@gmail.com>
2024-12-11 14:42:42 +0100

    corrección errores estilos

--- lang/en/block_ideal_users_map.php
+++ lang/en/block_ideal_users_map.php
@@ -2,10 +2,10 @@
 $string['pluginname'] = 'Ideal Users Map';
 $string['welcome'] = 'Welcome to the Ideal Users Map block!';
 $string['count_user_map'] = 'Users: ';
-$string['all_users'] = 'Total Users: ';
+$string['all_users'] = 'Total connected users: ';
 $string['connected_from'] = 'Connected from: ';
 $string['total_hub'] = 'Ideal user Hub   ';
-$string['user_for_country'] = 'User for country: ';
+$string['user_for_country'] = 'Users per Country: ';
 
 
 
